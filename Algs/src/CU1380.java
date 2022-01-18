90ed4fc30f4785d6cbf2e8d3a1cb1c3b9a4f3c7f
Taegyeong Jang <Taegyeong Jang@LAPTOP-Q7LRPPQ8>
2022-01-18 17:32:16 +0900

    중첩 반복문

---
+++ Algs/src/CU1380.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CU1380 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int k = sc.nextInt();
+
+		for (int i = 1; i < k; i++) {
+
+//			k=3; 1 2; 2 1; i/k-i
+//			k=4; 1 3; 2 2; 3 1;
+			if ((k-i > 6) || i > 6)
+				continue;
+
+			System.out.printf("%d %d\n", i, k - i);
+		}
+	}
+}
